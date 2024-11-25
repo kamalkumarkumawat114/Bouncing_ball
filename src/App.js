948dcf500d780e66fc6a00ea14b19eeb1033ce68
kamalkumarkumawat114 <kamalkumarkumawat114@gmail.com>
2024-11-25 12:07:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React, { useEffect, useRef } from 'react';
+import Phaser from 'phaser';
+import { gameConfig } from './config';
 import './App.css';
-import PhaserComponent from './Phaser';
 
 function App() {
-  const handleButtonClick = (position) => {
-    window.dispatchEvent(new CustomEvent('moveBall', { detail: position }));
+  const gameRef = useRef(null);
+
+  useEffect(() => {
+    gameRef.current = new Phaser.Game(gameConfig);
+
+    return () => {
+      if (gameRef.current) {
+        gameRef.current.destroy(true);
+      }
+    };
+  }, []);
+
+  const handleButtonClick = (direction) => {
+    const scene = gameRef.current?.scene.scenes[0];
+    if (scene) {
+      scene.bounceTowards(direction);
+    }
   };
 
   return (
-    <div className="App">
-      <div className="grid">
-        <button onClick={() => handleButtonClick('top-left')}>Top Left</button>
-        <button onClick={() => handleButtonClick('top')}>Top</button>
-        <button onClick={() => handleButtonClick('top-right')}>Top Right</button>
-        <button onClick={() => handleButtonClick('left')}>Left</button>
-        <div className="phaser-container">
-          <PhaserComponent />
+    <div className="game-container">
+      <div className="top-buttons">
+        <button onClick={() => handleButtonClick('topLeft')}>Top 1</button>
+        <button onClick={() => handleButtonClick('topRight')}>Top 2</button>
+      </div>
+
+      <div className="middle-section">
+        <div className="side-buttons left">
+          <button onClick={() => handleButtonClick('leftTop')}>Left 1</button>
+          <button onClick={() => handleButtonClick('leftBottom')}>Left 2</button>
         </div>
-        <button onClick={() => handleButtonClick('right')}>Right</button>
-        <button onClick={() => handleButtonClick('bottom-left')}>Bottom Left</button>
-        <button onClick={() => handleButtonClick('bottom')}>Bottom</button>
-        <button onClick={() => handleButtonClick('bottom-right')}>Bottom Right</button>
+
+        <div id="phaser-container" className="phaser-container"></div>
+
+        <div className="side-buttons right">
+          <button onClick={() => handleButtonClick('rightTop')}>Right 1</button>
+          <button onClick={() => handleButtonClick('rightBottom')}>Right 2</button>
+        </div>
+      </div>
+
+      <div className="bottom-buttons">
+        <button onClick={() => handleButtonClick('bottomLeft')}>Bottom 1</button>
+        <button onClick={() => handleButtonClick('bottomRight')}>Bottom 2</button>
       </div>
     </div>
   );
